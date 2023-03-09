4a4a8d1519abaf2339e1bd410e517f3aef5ba62d
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-03-09 18:25:42 +0100

    refactor(index): use wizard command factory and a temporary git command

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
+import { log } from "@clack/prompts";
 import { Command } from "commander";
-
-import { WizardCommand } from "./wizard";
+import {
+  getStagedDiff,
+  getStagedFiles,
+  isInsideGitRepo,
+} from "./git/helper/git-helper";
+import { WizardCommandFactory } from "./wizard/factories/wizard-command-factory";
 
 const program = new Command();
 
@@ -10,6 +15,32 @@ program
   .usage("[command] [options]")
   .helpOption("-h, --help", "Display help for command");
 
-program.addCommand(new WizardCommand());
+program.addCommand(
+  program.createCommand("git").action(async () => {
+    if (!(await isInsideGitRepo())) {
+      log.error("You are not inside a git repository");
+      log.info("Please run this command inside a git repository");
+      return;
+    }
+    log.info("Getting staged files...");
+    const stagedFiles = await getStagedFiles();
+    if (!stagedFiles) {
+      log.info("No staged files found");
+      return;
+    }
+    log.success(stagedFiles.join("\n"));
+    log.info("Generating diff...");
+    for (const file of stagedFiles) {
+      log.info(`Generating diff for ${file}...`);
+      const diff = await getStagedDiff(file);
+      if (!diff) {
+        log.info("No diff found");
+        return;
+      }
+      log.success(diff);
+    }
+  })
+);
+program.addCommand(WizardCommandFactory.create());
 
 program.parse(process.argv);
