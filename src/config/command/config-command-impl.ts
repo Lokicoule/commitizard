12286db5ee0e32b1fe973fa52a0419c01fbaed0d
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-03-11 14:34:48 +0100

    refactor(config): remove useless helper

--- src/config/command/config-command-impl.ts
+++ src/config/command/config-command-impl.ts
@@ -1,33 +1,10 @@
 import { Command } from "commander";
-import { Config, loadConfig, writeUserConfig } from "../config-helper";
+import { loadConfig, writeUserConfig } from "../config-helper";
 import { ConfigCommand } from "../interface/config-command";
 
-type SafeConfig = Omit<Config, "apiKey">;
-
-class ConfigHelper {
-  constructor() {}
-
-  public getConfig(): SafeConfig {
-    const { apiKey, ...safeConfig } = loadConfig();
-    return safeConfig;
-  }
-
-  public writeConfig(config: SafeConfig, configPath?: string): void {
-    writeUserConfig(config, configPath);
-  }
-}
-
 export class ConfigCommandImpl extends Command implements ConfigCommand {
-  private readonly configHelper: ConfigHelper;
-
-  constructor() {
-    super();
-    this.configHelper = new ConfigHelper();
-  }
-
   public async run(configPath?: string): Promise<void> {
-    const config = this.configHelper.getConfig();
-    console.log(config);
-    this.configHelper.writeConfig(config, configPath);
+    const { apiKey, ...safeConfig } = loadConfig();
+    writeUserConfig(safeConfig, configPath);
   }
 }
