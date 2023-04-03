f38b5213f1e7a4286586fd2f146810e0ce0d59b7
Loïk Fekkaï <lokicoule@yahoo.fr>
2023-04-03 10:50:27 +0200

    test(configuration): default values configuration manager

--- src/core/configuration/manager/impl/ConfigurationManagerImpl.spec.ts
+++ src/core/configuration/manager/impl/ConfigurationManagerImpl.spec.ts
@@ -168,4 +168,157 @@ describe("ConfigurationManagerImpl", () => {
       configurationManagerWithoutMaxViewFilesToShow.getWizardMaxViewFilesToShow()
     ).toBe(MAX_FILES_TO_SHOW);
   });
+
+  it("should return default value if conventional commit template is not set", () => {
+    const configWithoutConventionalCommitTemplate: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutConventionalCommitTemplate =
+      new ConfigurationManagerImpl(configWithoutConventionalCommitTemplate);
+
+    expect(
+      configurationManagerWithoutConventionalCommitTemplate.getConventionalCommitTemplate()
+    ).toBeUndefined();
+  });
+
+  it("should return default value if conventional commit template order is not set", () => {
+    const configWithoutConventionalCommitTemplateOrder: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutConventionalCommitTemplateOrder =
+      new ConfigurationManagerImpl(
+        configWithoutConventionalCommitTemplateOrder
+      );
+
+    expect(
+      configurationManagerWithoutConventionalCommitTemplateOrder.getConventionalCommitTemplateOrder()
+    ).toEqual([]);
+  });
+
+  it("should return default value if conventional cli options types is not set", () => {
+    const configWithoutConventionalCliOptionsTypes: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutConventionalCliOptionsTypes =
+      new ConfigurationManagerImpl(configWithoutConventionalCliOptionsTypes);
+
+    expect(
+      configurationManagerWithoutConventionalCliOptionsTypes.getConventionalCliOptionsTypes()
+    ).toEqual([]);
+  });
+
+  it("should return default value if conventional cli options scopes is not set", () => {
+    const configWithoutConventionalCliOptionsScopes: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutConventionalCliOptionsScopes =
+      new ConfigurationManagerImpl(configWithoutConventionalCliOptionsScopes);
+
+    expect(
+      configurationManagerWithoutConventionalCliOptionsScopes.getConventionalCliOptionsScopes()
+    ).toEqual([]);
+  });
+
+  it("should return default value if red green refactor commit template is not set", () => {
+    const configWithoutRedGreenRefactorCommitTemplate: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutRedGreenRefactorCommitTemplate =
+      new ConfigurationManagerImpl(configWithoutRedGreenRefactorCommitTemplate);
+
+    expect(
+      configurationManagerWithoutRedGreenRefactorCommitTemplate.getRedGreenRefactorCommitTemplate()
+    ).toBeUndefined();
+  });
+
+  it("should return default value if red green refactor commit template order is not set", () => {
+    const configWithoutRedGreenRefactorCommitTemplateOrder: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutRedGreenRefactorCommitTemplateOrder =
+      new ConfigurationManagerImpl(
+        configWithoutRedGreenRefactorCommitTemplateOrder
+      );
+
+    expect(
+      configurationManagerWithoutRedGreenRefactorCommitTemplateOrder.getRedGreenRefactorCommitTemplateOrder()
+    ).toEqual([]);
+  });
+
+  it("should return default value if red green refactor cli options types is not set", () => {
+    const configWithoutRedGreenRefactorCliOptionsTypes: Configuration = {
+      version: "1.0.0",
+      settings: {},
+      conventional: {
+        commitOptions: {} as ConventionalCommitTemplate,
+        cliOptions: {},
+      },
+      redGreenRefactor: {
+        commitOptions: {} as RedGreenRefactorCommitTemplate,
+        cliOptions: {},
+      },
+    } as unknown as Configuration;
+    const configurationManagerWithoutRedGreenRefactorCliOptionsTypes =
+      new ConfigurationManagerImpl(
+        configWithoutRedGreenRefactorCliOptionsTypes
+      );
+
+    expect(
+      configurationManagerWithoutRedGreenRefactorCliOptionsTypes.getRedGreenRefactorCliOptionsTypes()
+    ).toEqual([]);
+  });
 });
